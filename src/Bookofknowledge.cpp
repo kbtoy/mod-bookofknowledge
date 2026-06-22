e7d8bd82a90e4d704a657d9f4209b4a929c75bee
Kenny <kbsjeep@gmail.com>
2026-06-21 23:05:37 -0500

    Fixed unused parameter  compliation error

--- src/Bookofknowledge.cpp
+++ src/Bookofknowledge.cpp
@@ -261,7 +261,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+    bool OnGossipSelect(Player* player, Creature* /*creature*/, uint32 /*sender*/, uint32 action) override
     {
         if (action == ACTION_OPEN_UI)
         {
